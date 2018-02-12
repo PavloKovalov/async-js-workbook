68170b76e2a1f9a568b3fa2e4543c180869fe3c2
Pavel Kovalyov <sxe.funky@gmail.com>
2018-02-12 14:25:31 +0300

    Delete gh_config.js

--- gh_config.js
+++
@@ -1,8 +0,0 @@
-// https://goo.gl/rcMDkj
-const GH_CLIENT_ID = 'e39def60055d1ddf6dcb';
-const GH_CLIENT_SECRET = '51a8e9a6b31abe0f053f91a0959860ae0981a369';
-
-export {
-  GH_CLIENT_ID,
-  GH_CLIENT_SECRET
-};
